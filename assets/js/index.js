2e5d56cea710e1f9d2d78738b43b810e9d6a731d
surajverma2587 <surajverma2587@gmail.com>
2022-04-25 21:25:17 +0100

    go to next question on option click

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,6 +49,62 @@ const questions = [
   },
 ];
 
+// event handler function to handle click events in question section
+const handleOptionClick = (event) => {
+  console.log("clicked somewhere in question section");
+
+  // get current target
+  const currentTarget = event.currentTarget;
+
+  // get target
+  const target = event.target;
+
+  // check if click originates from li only
+  // check if target element is li element
+  if (target.tagName === "LI") {
+    // get the option the user clicked on
+    const value = target.getAttribute("data-value");
+
+    // get the question the user answered
+    const question = questions[questionIndex].text;
+
+    // build an answer object that contains question and answer
+    const answer = {
+      question,
+      value,
+    };
+
+    // TODO: store answer in local storage
+    console.log(answer);
+
+    // remove question
+    removeQuestion();
+
+    if (questionIndex < questions.length - 1) {
+      // go to next question if not the last question
+      // increment the question index by 1
+      questionIndex += 1;
+
+      // render question
+      renderQuestion();
+    } else {
+      // if last question then render results and form
+      renderResults();
+      renderForm();
+    }
+  }
+};
+
+// function to render the results
+const renderResults = () => {
+  console.log("render results");
+};
+
+// function to render the form
+const renderForm = () => {
+  console.log("render form");
+};
+
 // function to render question to page
 const renderQuestion = () => {
   console.log("render question");
@@ -59,6 +115,7 @@ const renderQuestion = () => {
   // create section
   const section = document.createElement("section");
   section.setAttribute("class", "content-section question-container");
+  section.setAttribute("id", "question-container");
 
   // create h2
   const h2 = document.createElement("h2");
@@ -73,14 +130,17 @@ const renderQuestion = () => {
   // TODO: loop over options to create and append li to ul
   const li1 = document.createElement("li");
   li1.setAttribute("class", "list-item");
+  li1.setAttribute("data-value", currentQuestion.options[0]);
   li1.textContent = currentQuestion.options[0];
 
   const li2 = document.createElement("li");
   li2.setAttribute("class", "list-item");
+  li2.setAttribute("data-value", currentQuestion.options[1]);
   li2.textContent = currentQuestion.options[1];
 
   const li3 = document.createElement("li");
   li3.setAttribute("class", "list-item");
+  li3.setAttribute("data-value", currentQuestion.options[2]);
   li3.textContent = currentQuestion.options[2];
 
   ul.append(li1, li2, li3);
@@ -90,12 +150,21 @@ const renderQuestion = () => {
 
   // append question section to main element
   mainElement.append(section);
+
+  // add event listener on question section
+  section.addEventListener("click", handleOptionClick);
 };
 
 // function to remove banner from page
 const removeBanner = () => {
   console.log("remove banner");
   bannerSection.remove();
+};
+
+// function to remove question section from page
+const removeQuestion = () => {
+  console.log("remove question");
+  document.getElementById("question-container").remove();
 };
 
 // declare the event handler function for start button click
