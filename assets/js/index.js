d1693f886faa02d7cedbe4eee5b306c1108a0942
surajverma2587 <surajverma2587@gmail.com>
2022-04-25 19:36:23 +0100

    add psuedo code for start btn click

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,29 @@
-console.log("hello from index");
+// target start button
+const startButton = document.getElementById("start-btn");
+
+// target banner section
+const bannerSection = document.getElementById("banner");
+
+// function to render question to page
+const renderQuestion = () => {
+  console.log("render question");
+};
+
+// function to remove banner from page
+const removeBanner = () => {
+  console.log("remove banner");
+};
+
+// declare the event handler function for start button click
+const handleStartButtonClick = () => {
+  console.log("start button clicked");
+
+  // remove banner section
+  removeBanner();
+
+  // render question
+  renderQuestion();
+};
+
+// add event listener to start button
+startButton.addEventListener("click", handleStartButtonClick);
